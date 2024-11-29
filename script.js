186b1b61bad4351ff3be8224d08a04a0544d7347
Bodya <bhhuuu18@gmail.com>
2024-11-29 16:55:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ const select = document.querySelector("select#lesson-picker");
 const iframe = document.querySelector("iframe");
 const downloadBtn = document.querySelector(".wrapper > a");
 
+const lessonMaterials = {
+    "2": { text: "lesson 2", href: "Архив_для_урока_2.zip" },
+    "3": { text: "lesson 3", href: "Макет_для_урока_3.zip" },
+    "Бонус": { text: "bonus lesson", href: "Макет_для_урока_3.zip" }
+};
+
 select.addEventListener("change", () => {
-    iframe.setAttribute("src", getLesson(select.value));
-    // if (select.value === 2) {downloadBtn.innerHTML = "Download materials for lesson 2";};
+    const selectedValue = select.value;
+    iframe.setAttribute("src", getLesson(selectedValue));
+
+    if (lessonMaterials[selectedValue]) {
+        downloadBtn.style.display = "block";
+        downloadBtn.innerHTML = "Download materials for " + lessonMaterials[selectedValue].text;
+        downloadBtn.setAttribute("href", "materials/" + lessonMaterials[selectedValue].href);
+    } else {
+        downloadBtn.style.display = "none";
+    }
 });
 
 function getLesson(value) {
@@ -16,6 +30,6 @@ function getLesson(value) {
         case "5": return "https://www.youtube.com/embed/jaMkWpfymOc";
         case "6": return "https://www.youtube.com/embed/nXa9aexl6AI";
         case "7": return "https://www.youtube.com/embed/BCCX9mCVWEE";
-        case "Бонус": return "https://www.youtube.com/embed/lDk-1etgVr8";
+        case "Бонус": return "https://www.youtube.com/embed/lDk-1etgVr8"
     }
 }
